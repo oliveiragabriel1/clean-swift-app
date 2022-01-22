cb41336c616efeba92ed946467e2c4e4680d4221
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-01-22 18:38:04 -0300

    feat: rename HttpClient protocol to HttpPostClient

--- DataTests/RemoteAddAccount.swift
+++ DataTests/RemoteAddAccount.swift
@@ -10,8 +10,8 @@ import XCTest
 
 class RemoteAddAccount {
     private let url : URL
-    private let httpClient : HttpClient
-    init (url: URL, httpClient : HttpClient ) {
+    private let httpClient : HttpPostClient
+    init (url: URL, httpClient : HttpPostClient ) {
         self.url = url
         self.httpClient = httpClient
     }
@@ -20,7 +20,7 @@ class RemoteAddAccount {
     }
 }
 
-protocol HttpClient {
+protocol HttpPostClient {
     func post (url: URL)
 }
 
@@ -37,7 +37,7 @@ class RemoteAddAccountTests: XCTestCase {
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
-    class HttpClientSpy: HttpClient {
+    class HttpClientSpy: HttpPostClient {
         var url: URL?
         
         func post(url: URL) {
