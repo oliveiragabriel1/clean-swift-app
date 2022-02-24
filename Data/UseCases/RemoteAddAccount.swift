461186cf3819b2016a22133d774d58f4c76d661a
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-24 18:04:20 -0300

    feat: ensure HttpPostClient not complete when RemoteAddAccount has been deadlocked

--- Data/UseCases/RemoteAddAccount.swift
+++ Data/UseCases/RemoteAddAccount.swift
@@ -10,7 +10,8 @@ public final class RemoteAddAccount: AddAccount {
         self.httpClient = httpClient
     }
     public func add (addAccountModel: AddAccountModel, completion: @escaping(Result<AccountModel, DomainError>) -> Void) {
-        httpClient.post(to: url, with: addAccountModel.toData()) { result in
+        httpClient.post(to: url, with: addAccountModel.toData()) { [weak self] result in
+            guard self != nil else { return }
             switch result {
             case .success(let data):
                 if let model: AccountModel = data.toModel(){
@@ -24,5 +25,6 @@ public final class RemoteAddAccount: AddAccount {
         }
     }
 }
+
 
 
--- DataTests/UseCases/RemoteAddAccount.swift
+++ DataTests/UseCases/RemoteAddAccount.swift
@@ -1,9 +1,4 @@
-//
-//  DataTests.swift
-//  DataTests
-//
-//  Created by Gabriel Oliveira on 22/01/22.
-//
+
 
 import XCTest
 import Domain
@@ -47,14 +42,25 @@ class RemoteAddAccountTests: XCTestCase {
         })
         
     }
-    
+  
     func test_add_should_complete_with_error_if_client_complete_with_invalid_data() {
 
         let (sut, httpClientSpy) = makeSut()
         expect(sut, completeWith: .failure(.unexpected), when: {
             httpClientSpy.completeWithData(makeInvalidData())
         })
-       
+    }
+    
+    
+    func test_add_should_not_complete_if_sut_has_been_deallocated() {
+
+        let httpClientSpy = HttpClientSpy()
+        var sut: RemoteAddAccount? = RemoteAddAccount(url: makeUrl(), httpClient: httpClientSpy )
+        var result: Result<AccountModel, DomainError>?
+        sut?.add(addAccountModel: makeAddAccountModel()){ result = $0 }
+        sut = nil
+        httpClientSpy.completeWithError(.noConectivity)
+        XCTAssertNil(result)
         
     }
     
