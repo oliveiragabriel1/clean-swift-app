bd415919efda0e2cb348cc1b958874bb23c29e85
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-26 11:19:01 -0300

    feat: add AddAccountIntegration tests

---
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -0,0 +1,34 @@
+
+import XCTest
+import Data
+import Infra
+import Domain
+
+
+class AddAccountIntegrationTests: XCTestCase {
+
+     func test_add_account() {
+         let alamofireAdapter = AlamofireAdapter()
+         let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
+         let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+         let addAccountModel = AddAccountModel(name: "Gabriel Oliveira", email: "gabrieloliveira@gmail.com", password: "secret", passwordConfirmation: "secret")
+         
+         let exp = expectation(description: "waiting")
+         sut.add(addAccountModel: addAccountModel) { result in
+             
+             switch result {
+             case .failure: XCTFail("Expect success got \(result) instead")
+             case .success(let account):
+                 XCTAssertNotNil(account.id)
+                 XCTAssertEqual(account.name, addAccountModel.name)
+                 XCTAssertEqual(account.email, addAccountModel.email)
+                
+             }
+             exp.fulfill()
+         }
+         wait(for: [exp], timeout: 15)
+     }
+
+   
+
+}
