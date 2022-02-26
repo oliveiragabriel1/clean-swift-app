2fa5db6bf16fffb09e5b7744cc46c1c43941065e
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-26 19:08:35 -0300

    feat: move validation to a method

--- PresentationTests/SignUpPresenterTests.swift
+++ PresentationTests/SignUpPresenterTests.swift
@@ -13,16 +13,23 @@ class SignUpPresenter {
         self.alertView = alertView
     }
     func signUp(viewModel: SignUpViewModel) {
-        if(viewModel.name == nil || viewModel.name!.isEmpty){
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo Nome é obrigatório."))
-        } else if(viewModel.email == nil || viewModel.email!.isEmpty){
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo Email é obrigatório."))
-        } else if(viewModel.password == nil || viewModel.password!.isEmpty){
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo Senha é obrigatório."))
-        } else if(viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty){
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo Confirmar Senha é obrigatório."))
-        }
         
+        if let message = validade(viewModel: viewModel) {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
+        }
+    }
+    
+    private func validade(viewModel: SignUpViewModel) -> String? {
+        if(viewModel.name == nil || viewModel.name!.isEmpty){
+            return "O campo Nome é obrigatório."
+        } else if(viewModel.email == nil || viewModel.email!.isEmpty){
+            return "O campo Email é obrigatório."
+        } else if(viewModel.password == nil || viewModel.password!.isEmpty){
+            return "O campo Senha é obrigatório."
+        } else if(viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty){
+            return "O campo Confirmar Senha é obrigatório."
+        }
+        return nil
     }
 }
 
