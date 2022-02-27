f440a7566ab35ae27066e379fb42bc8c5403d756
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-27 17:05:05 -0300

    feat: add Storyboarded protocol to simplify ViewController creation

--- UITests/Scenes/SignUp/SignUpViewControllerTests.swift
+++ UITests/Scenes/SignUp/SignUpViewControllerTests.swift
@@ -34,8 +34,8 @@ class SignUpViewControllerTests: XCTestCase {
 extension SignUpViewControllerTests {
     
     func makeSut(signUpSpy: ((SignUpViewModel) -> Void)? = nil) -> SignUpViewController {
-        let sb = UIStoryboard(name: "SignUp", bundle:  Bundle(for: SignUpViewController.self))
-        let sut = sb.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
+        
+        let sut = SignUpViewController.instantiate()
         sut.signUp = signUpSpy
         sut.loadViewIfNeeded()
         
