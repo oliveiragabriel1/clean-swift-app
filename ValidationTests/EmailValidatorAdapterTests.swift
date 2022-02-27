14dd8f1580008622fe9cfb4f4442d042e1b872cb
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-27 17:15:00 -0300

    feat: add validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Gabriel Oliveira on 27/02/22.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+
+    func test_() throws {
+        
+    }
+
+}
