5d27a9bea77de413f705d2b092cd8d3e29a7491f
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-27 16:55:32 -0300

    feat: close keyboard on view controller tap

---
+++ UI/Helpers/UIViewControllerExtentions.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardOnTap() {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        gesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(gesture)
+    }
+    
+    @objc private func hideKeyboard() {
+        view.endEditing(true)
+    }
+}
--- UI/Scenes/SignUp/SignUpViewController.swift
+++ UI/Scenes/SignUp/SignUpViewController.swift
@@ -22,8 +22,8 @@ final class SignUpViewController: UIViewController {
     private func configure() {
         saveButton?.layer.cornerRadius = 5
         saveButton?.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+        hideKeyboardOnTap()
     }
-    
     @objc private func saveButtonTapped() {
         let viewModel = SignUpViewModel(name: nameTextField?.text, email: emailTextField?.text, password: passwordTextField?.text, passwordConfirmation: passwordConfirmationTextField?.text)
         signUp?(viewModel)
