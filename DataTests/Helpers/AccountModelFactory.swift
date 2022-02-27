e1bc05b8ba9aa998b54e81f69bcea3c566e68139
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-27 18:34:47 -0300

    refactor: change AccountModel to have only an accessToken

--- DataTests/Helpers/AccountModelFactory.swift
+++ DataTests/Helpers/AccountModelFactory.swift
@@ -2,7 +2,7 @@ import Foundation
 import Domain
 
 func makeAccountModel() -> AccountModel {
-    return AccountModel(id: "any_id", name: "any_name", email: "any_email@email.com", password: "any_password")
+    return AccountModel(accessToken: "any_token")
 }
 func makeAddAccountModel() -> AddAccountModel {
     return AddAccountModel(name: "any_name", email: "any_email@email.com", password: "any_password", passwordConfirmation: "any_password")
--- Domain/Models/AccountModel.swift
+++ Domain/Models/AccountModel.swift
@@ -1,22 +1,11 @@
-//
-//  AccountModel.swift
-//  Domain
-//
-//  Created by Gabriel Oliveira on 22/01/22.
-//
 
 import Foundation
 
 public struct AccountModel:Model {
-    public var id: String
-    public var name: String
-    public var email: String
-    public var password: String
+    public var accessToken: String
+
     
-    public init( id: String, name: String, email: String, password: String){
-        self.id = id
-        self.name = name
-        self.email = email
-        self.password = password
+    public init(accessToken: String){
+        self.accessToken = accessToken
     }
 }
