df3d375c31a49dc8f4b24f0ae75b9b53f8392c06
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-27 16:17:58 -0300

    refactor: move UI extention to a separate file

---
+++ UITests/Helpers/TestExtentions.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
