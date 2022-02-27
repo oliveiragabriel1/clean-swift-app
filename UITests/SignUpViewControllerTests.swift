4cac6bc277a52b3f54eaf68f0f1609fd9aab0212
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-27 15:23:12 -0300

    feat: add test to guarantee that SignUpViewController implements LoadingView

--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 import UIKit
+import Presentation
 @testable import UI
 
 class SignUpViewControllerTests: XCTestCase {
@@ -11,4 +12,10 @@ class SignUpViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
     }
 
+    func test_sut_implements_loadingView() {
+        let sb = UIStoryboard(name: "SignUp", bundle:  Bundle(for: SignUpViewController.self))
+        let sut = sb.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
+        
+        XCTAssertNotNil(sut as LoadingView)
+    }
 }
