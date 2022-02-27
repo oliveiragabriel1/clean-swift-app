99291f991255fc9120088af5493566d16d9cf7f8
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-27 17:25:15 -0300

    feat: ensure EmailValidatorAdaptor returns false if an invalid email is provided

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -1,17 +1,24 @@
-//
-//  ValidationTests.swift
-//  ValidationTests
-//
-//  Created by Gabriel Oliveira on 27/02/22.
-//
-
 import XCTest
+import Presentation
 
+public final class EmailValidatorAdapter: EmailValidator {
+    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    public func isValid(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.utf16.count)
+        let regex = try! NSRegularExpression(pattern: pattern)
+        return regex.firstMatch(in: email, options: [], range: range) != nil
+    }
+}
 class EmailValidatorAdapterTests: XCTestCase {
 
 
-    func test_() throws {
-        
+    func test_invalid_emails() {
+        let sut = EmailValidatorAdapter()
+        XCTAssertFalse(sut.isValid(email: "RR"))
+        XCTAssertFalse(sut.isValid(email: "rr@"))
+        XCTAssertFalse(sut.isValid(email: "rr@rr"))
+        XCTAssertFalse(sut.isValid(email: "rr@rr."))
+        XCTAssertFalse(sut.isValid(email: "@rr.com"))
     }
 
 }
