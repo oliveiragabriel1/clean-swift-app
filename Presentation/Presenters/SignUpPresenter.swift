4a469dc5ba59f40113028b57972e2b40cbf0c992
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-27 11:25:34 -0300

    feat: ensure SignUpPresenter hides loading after addAccount completes

--- Presentation/Presenters/SignUpPresenter.swift
+++ Presentation/Presenters/SignUpPresenter.swift
@@ -28,6 +28,7 @@ public class SignUpPresenter {
                 case .failure: self.alertView.showMessage(viewModel: AlertViewModel(title: "Erro", message: "Algo inesperado aconteceu, tente novamente em alguns instantes."))
                 case.success: break
                 }
+                self.loadingView.display(viewModel: LoadingViewModel(isLoading: false))
             }
         }
         
